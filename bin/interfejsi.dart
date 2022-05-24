56c125847850996db11bad742c182f51cea3ed79
Patrik Pralas <patrik.pralas@q.agency>
2022-05-24 09:51:05 +0200

    interfejs update

--- bin/interfejsi.dart
+++ bin/interfejsi.dart
@@ -1,1 +1,35 @@
-abstract class
+/*
+
+abstract class DataRepository {
+  factory DataRepository() => FakeWebServer();
+  double? fetchTemperature(String city);
+}
+
+class FakeWebServer implements DataRepository {
+  @override
+  double? fetchTemperature(String city) {
+    return 42.0;
+  }
+}
+*/
+
+void bottleExercise() {
+  final bottle = Bottle();
+  bottle.open();
+}
+
+abstract class Bottle {
+  factory Bottle() => SodaBottle();
+  void open();
+}
+
+class SodaBottle implements Bottle {
+  @override
+  void open() {
+    print('fsssssss');
+  }
+}
+
+void main() {
+  bottleExercise();
+}
