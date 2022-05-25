6dfb613e168f915c446ed5f1cfdb6f814aed11ef
Patrik Pralas <patrik.pralas@q.agency>
2022-05-25 13:05:56 +0200

    finished task

--- bin/football/midfielder.dart
+++ bin/football/midfielder.dart
@@ -1,8 +1,20 @@
 // import 'player.dart';
 
-// class Midfielder extends Player {
-//   Midfielder(String name, int speed, int strenght, int stamnia)
-//       : super(name, speed, strenght, stamnia);
+import 'player.dart';
 
-//   var position = 'Midfielder';
-// }
+class Midfielder extends Player {
+  Midfielder({String? name, int? speed, int? strenght, int? stamina})
+      : super(name: name, speed: speed, strenght: strenght, stamina: stamina);
+
+  void run() {
+    speed = speed! + 2;
+    stamina = stamina! - 3;
+  }
+
+  void rest() {
+    stamina = stamina! + 5;
+    speed = 0;
+  }
+  // cmd + slash i komentar
+
+}
--- bin/football/player.dart
+++ bin/football/player.dart
@@ -23,9 +23,35 @@ void main() {
     stamina: 30,
   );
 
+  final dominik = Defender(
+    name: 'Dominik',
+    speed: 30,
+    strenght: 70,
+    stamina: 10,
+  );
+
+  final adrijan = Midfielder(
+    name: 'Adrijan',
+    speed: 60,
+    strenght: 40,
+    stamina: 92,
+  );
+
   print(milan.toString());
   milan.run();
   print(milan.toString());
   milan.rest();
   print(milan.toString());
+
+  print(dominik.toString());
+  dominik.run();
+  print(dominik.toString());
+  dominik.rest();
+  print(dominik.toString());
+
+  print(adrijan.toString());
+  adrijan.run();
+  print(adrijan.toString());
+  adrijan.rest();
+  print(adrijan.toString());
 }
